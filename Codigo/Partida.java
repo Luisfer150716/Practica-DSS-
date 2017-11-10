ba4f0e235830f329c4af21357b0047fe3b8ea042
aronsalinas96 <32863687+aronsalinas96@users.noreply.github.com>
2017-11-10 11:19:10 +0100

    Update Partida.java

--- Codigo/Partida.java
+++ Codigo/Partida.java
@@ -1,16 +1,21 @@
 class Partida{
-	private int n_jugadores; //obligatorio para cualquier juego
-	private Jugador[] jugadores; // "" 				 ""
-	private casillaMonopoly[] tablero; //para un caso particular;
-	private double dineroBanca = Double.POSITIVE_INFINITY;
-	private long duracionPartida ; // para partida con tiempo
-	private long tiempoTranscurrido;
+	private final int n_jugadores; //obligatorio para cualquier juego
+	private final Jugador[] jugadores; // "" 				 ""
+	private final casillaMonopoly[] tablero; //para un caso particular;
+	private final double dineroBanca = Double.POSITIVE_INFINITY;
+	private final long duracionPartida ; // para partida con tiempo
+	private final long tiempoTranscurrido;
 	private Partida(BuilderPartida builderp){
-		this.tablero = builderp.getTablero();
-		this.n_jugadores = builderp.getNJugadores();
+		this.tablero = builderp.tablero;
+		this.n_jugadores = builderp.n_jugadores
 		jugadores = new Jugador[n_jugadores];
-		this.jugadores = builderp.getJugadores();
-		this.duracionPartida = builderp.getDuracion();
+		this.jugadores = builderp.jugadores;
+		this.duracionPartida = builderp.duracionPartida;
 
 	}
+	public int getNJugadores(){return this.n_jugadores;}
+	public Jugador[] getJugadores(){return this.jugadores;}
+	public casillaMonopoly[] getTablero(){return this.tablero;}
+	public long duracionPartida(){return this.duracionPartida;}
+	public long tiempoTranscurrido(){return  //implementar tiempo actual - tiempo inicio;}
 };
