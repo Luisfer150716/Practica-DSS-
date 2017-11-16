8f5f9cfeaf7df3ff1d5b2430b4e09c817efdcf32
aronsalinas96 <32863687+aronsalinas96@users.noreply.github.com>
2017-11-16 12:20:16 +0100

    Update Carcel.java
    Hemos modificado su comportamiento,ahora se decrementan los turnos

--- Codigo/Carcel.java
+++ Codigo/Carcel.java
@@ -1,10 +1,23 @@
 public class Carcel extends CasillaMonopoly{
-	int turnos;
-	boolean visita;
+	int turnos = new int[4];
+	boolean visita = new boolean[4];
 
 	public Carcel(String nombre, int turnos, boolean visita){
 		super(nombre);
 		this.turnos = turnos;
 		this.visita = visita;
 	}
+	public void accion(Jugador j){
+		if(!j.getCarcel()){
+			System.out.println("Estas de visita!!");	
+		}
+		else{
+			System.out.println("Estas en la carcel, te quedan "+turnos[j.getId()]+" turnos");
+			turnos[j.getId()]--;
+			if(turnos[j.getId()]==0){
+				j.setCarcel(false);	
+			}
+		}
+	}
+	
 }
