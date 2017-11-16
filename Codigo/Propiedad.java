f0749027aaa8b1ce53074c03a98fd1f3d65dcc4c
aronsalinas96 <32863687+aronsalinas96@users.noreply.github.com>
2017-11-16 12:22:36 +0100

    Update Propiedad.java

--- Codigo/Propiedad.java
+++ Codigo/Propiedad.java
@@ -2,10 +2,11 @@ public class Propiedad extends CasillaMonopoly{
 
 	int valor;
 	Jugador dueño;
-
+	
 	public Propiedad(String nombre, int valor){
 		super(nombre);
 		this.valor= valor;
 		this.dueño= null;
 	}
+	public void accion(Jugador j); //NO HACE NADA , SIEMPRE SE LLAMA A EDIFICABLE O NO EDIFICABLE
 }
