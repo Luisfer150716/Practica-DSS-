e1fdbe845a58e2cdd38d9db591a67519c2401f43
aronsalinas96 <32863687+aronsalinas96@users.noreply.github.com>
2017-11-23 11:11:27 +0100

    Update FabricaTablero.java
    Aplicado patron singleton

--- Codigo/FabricaTablero.java
+++ Codigo/FabricaTablero.java
@@ -1,10 +1,25 @@
 
 
 public class FabricaTablero{
-	Casilla vacia(String nombre);
-	Suerte nuevaSuerte(String nombre,int casillas,int dinero,);
-	Carcel nuevaCarcel(String nombre,int turnos);
-	Recompensa nuevaRecompensa(String nombre,int premio);
+	public static FabricaTablero instancia(String fabrica){
+		if(fabrica == "FabricaTableroMonopoly"){
+			_fabrica = new FabricaTableroMonopoly();	
+		}
+		else{
+			_fabrica = new FabricaTablero();	
+		}
+		return _fabrica;
+	}
+	public Casilla vacia(String nombre);
+	public Casilla nuevaSuerte(String nombre,int casillas,int dinero,);
+	public Casilla nuevaCarcel(String nombre,int turnos);
+	public Casilla nuevaRecompensa(String nombre,int premio);
+		
+	protected:
+		FabricaTablero();
+	private:
+		static FabricaTablero _fabrica = 0;
+	
 	
 
 };
