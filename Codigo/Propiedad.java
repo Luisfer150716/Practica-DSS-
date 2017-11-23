9e21df08032b423cb4bb31c411dcf7f7d95c8604
aronsalinas96 <32863687+aronsalinas96@users.noreply.github.com>
2017-11-23 12:16:47 +0100

    Update Propiedad.java
    Añadidos get set de dueño y valor;

--- Codigo/Propiedad.java
+++ Codigo/Propiedad.java
@@ -9,4 +9,13 @@ public class Propiedad extends CasillaMonopoly{
 		this.dueño= null;
 	}
 	public void accion(Jugador j); //NO HACE NADA , SIEMPRE SE LLAMA A EDIFICABLE O NO EDIFICABLE
+	public getValor(){
+		return this.valor;	
+	}
+	public Jugador getDueño(){
+		return this.dueño;	
+	}
+	public void setDueño(Jugador j){
+		this.dueño = j;	
+	}
 }
