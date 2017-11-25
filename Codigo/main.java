0484cee21b67911363b360cb0c2ff133d37c4dd3
aronsalinas96 <32863687+aronsalinas96@users.noreply.github.com>
2017-11-25 15:11:39 +0100

    Update main.java
    se delega el desarrollo de la partida y su configuracion a la nueva clase GestorPartida

--- Codigo/main.java
+++ Codigo/main.java
@@ -2,126 +2,30 @@
 
 public static void main(String[] args) {
 
-								
-							
-	
-	/*Scanner sc = new Scanner(System.in);
-	System.out.println("Introduce numero de jugadores: ");
-	int numJugadores = sc.nextInt();
-	Jugador[numJugadores] jugadores;
-
-	System.out.println("Comienza la partida");
-	System.out.println("-------------------");
-	System.out.println("Jugador " +jugadores[1].getNombre()+" tira los dados");
-	jugadores[1].lanzarDados();
-	System.out.println(jugadores[1].getNombre()+" se ha movido a la casilla: "+jugadores[1].getCasilla_actual());
-	//System.out.println("Que desea realizar: ");
-	if(tablero[jugadores[1].getCasilla_actual()] == )
-	System.out.println("1.");*/
-
+	GestorPartida gestor = GestorPartida.instancia();
+	gestor.muestraMenuInicial();
 	static Scanner sc = new Scanner(System.in);
-	System.out.println("Elige opcion: ");
-	System.out.println("1.Nueva partida");
-	System.out.println("2.Cargar partida");
-	System.out.println("3.Salir");
 	int opcion = sc.nextInt();
-
 	switch (opcion) {
-		case 1:	do{
-					System.out.println("Introduce numero de jugadores: ");
-					int numJugadores = sc.nextInt();
-					if(numJugadores < 2 || numJugadores > 4)
-						System.out.println("Numero de jugadores incorrectos.");
-
-				} while(numJugadores < 2 || numJugadores > 4)
-
-				PartidaMonopoly partida = new PartidaMonopoly(numJugadores);
-				partida.construyeTablero();
-				for(int i=1; i<=numJugadores; i++){
-					System.out.println("Introduce nombre del Jugador "+i);
-					String nombre = sc.nextString();
-					System.out.println("Introduce figura del Jugador "+i);
-					String figura = sc.nextString();
-					partida.jugadores.add(i,new Jugador(nombre, figura, 1, 500);
-					partida.jugadores.elementAt(i, this.setId(i);
-					
-				}
-
-				boolean seJuega=true;
-				int turno = 0;
-				int jugador = 1;
-				Jugador banca(banca, null, null, 999999);
-				while(seJuega){
-					System.out.println("Es el turno "+turno);
-					if(partida.jugadores.elementAt(jugador).getCarcel()){
-						tablero[10].accion(partida.jugadores.elementAt(jugador));
-					}else{
-						System.out.println("Tira el jugador "+jugador);
-						System.out.println("Dinero: " +partida.jugadores.elementAt(jugador).getDinero());
-						System.out.println("Casilla actual: "+partida.jugadores.elementAt(jugador).getCasilla_actual());
-						System.out.println("Propiedades edificables: ");
-						for(int j = 0; j <partida.jugadores.elementAt(jugador).propiedadesEdificables.size(); j++){
-							System.out.println(partida.jugadores.elementAt(jugador).propiedadesEdificables.elementAt(j).getNombre());
-							}
-						System.out.println("Propiedades no edificables: ");
-						for(int j = 0; j <partida.jugadores.elementAt(jugador).propiedadesNoEdificables.size(); j++){
-							System.out.println(partida.jugadores.elementAt(jugador).propiedadesNoEdificables.elementAt(j).getNombre());
-							}
-
-						System.out.println("Jugador " +partida.jugadores[jugador].getNombre()+" tira los dados");
-						partida.jugadores.elementAt(jugador).lanzarDados();
-						System.out.println(partida.jugadores.elementAt(jugador).getNombre()+" se ha movido a la casilla: "+partida.jugadores[jugador].getCasilla_actual());
-
-						partida.tablero[partida.jugadores.elementAt(jugador).getCasilla_actual()].accion(partida.jugadores.elementAt(jugador));
-						}
-
-					if(partida.jugadores.elementAt(jugador).getDinero()>0){	
-						jugador++;
-						if(jugador >numJugadores)
-							jugador = 1;
-						turno++;
-					}
-
-					else{
-						if(partida.jugadores.elementAt(jugador).propiedadesEdificables.isEmpty() && partida.jugadores.elementAt(jugador).propiedadesNoEdificables.isEmpty()){
-							partida.eliminarJugador(jugador);
-							
-						}
-						else{
-							mostrarPropiedades(partida.jugadores.elementAt(jugador));
-							int op = sc.nextInt();
-							if(op == 1){
-								System.out.println("Introduce el numero de la propiedad edificable que quieras eliminar");
-								int pr = sc.nextInt();
-								if(partida.jugadores.elementAt(jugador) != null){
-									partida.jugadores.elementAt(jugador).setDinero(	partida.jugadores.elementAt(jugador).getDinero() +
-														       partida.jugadores.elementAt(jugador).getEdificables.elementAt(pr).getValor());
-									
-									partida.propiedad_a_banca(banca,partida.jugadores.elementAt(jugador).getEdificables.elementAt(pr));
-									
-								}
-							}else{
-								System.out.println("Introduce el numero de la propiedad no edificable que quieras eliminar");
-								int pr = sc.nextInt();
-								partida.jugadores.elementAt(jugador).borrarNoEdificable(pr);
-							}
+		case 1:	
+			//CREAMOS TABLERO
+			PartidaMonopoly partida = new PartidaMonopoly();
+			//AÑADE JUGADORES
+			gestor.configurarPartida(partida);
+			//INICIA PARTIDA
+			gestor.desarrollaPartida(partida);
+			break;	
 				
-						}
-
-
-
-						///for(int j=0; j<partida.jugadores[jugador].propiedadesEdificables.length;j++){
-							///if(partida.jugadores[jugador].propiedadesEdificables[j]
-						}
-					}
-				}
-
-
-
-
 		case 2:
-		default : break;
+			System.out.println("Introduce el nombre de la partida que quieras cargar");
+			String nombre = sc.nextString();
+			PartidaMonopoly partidaCargada = gestor.cargarPartida(nombre);
+			gestor.desarrollaPartida(partida);
+			break;
+		case default:
+			System.out.printl("Poderoso caballero es Don Dinero, hasta luego!");
+
 	}
-
-
+		
+	
 }
