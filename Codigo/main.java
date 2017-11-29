25a6c7201789693fd03aabd4657c1e15b186f2ee
aronsalinas96 <32863687+aronsalinas96@users.noreply.github.com>
2017-11-29 22:05:02 +0100

    Update main.java
    delegado configuracion y desarrollo partida a la clase GestorPartida

--- Codigo/main.java
+++ Codigo/main.java
@@ -1,31 +1,42 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package monopolyclasico;
+import java.io.IOException;
+import java.util.Scanner;
 
 
-public static void main(String[] args) {
+    public static void main(String[] args) throws IOException, ClassNotFoundException {
+            FabricaTablero fabrica = FabricaTablero.instancia("FabricaMonopolyClasico");
+            GestorPartida gestor = GestorPartida.instancia();
+            gestor.muestraMenuInicial();
+            Scanner sc = new Scanner(System.in);
+            int opcion = sc.nextInt();
+            switch (opcion) {
+                    case 1:	
+                            //CREAMOS TABLERO
+                            PartidaMonopoly partida = new PartidaMonopoly(fabrica);
+                            //AÑADE JUGADORES
+                            gestor.configurarPartida(partida);
+                            //INICIA PARTIDA
+                            gestor.desarrollaPartida(partida);
+                            break;	
 
-	GestorPartida gestor = GestorPartida.instancia();
-	gestor.muestraMenuInicial();
-	static Scanner sc = new Scanner(System.in);
-	int opcion = sc.nextInt();
-	switch (opcion) {
-		case 1:	
-			//CREAMOS TABLERO
-			PartidaMonopoly partida = new PartidaMonopoly();
-			//AÑADE JUGADORES
-			gestor.configurarPartida(partida);
-			//INICIA PARTIDA
-			gestor.desarrollaPartida(partida);
-			break;	
-				
-		case 2:
-			System.out.println("Introduce el nombre de la partida que quieras cargar");
-			String nombre = sc.nextString();
-			PartidaMonopoly partidaCargada = gestor.cargarPartida(nombre);
-			gestor.desarrollaPartida(partida);
-			break;
-		case default:
-			System.out.printl("Poderoso caballero es Don Dinero, hasta luego!");
+                    case 2:
+                            System.out.println("Introduce el nombre de la partida que quieras cargar");
+                            String nombre = sc.next();
+                            Object partidaCargada = new PartidaMonopoly(fabrica);
+                            partidaCargada = gestor.cargarPartida(nombre);
+                            gestor.desarrollaPartida((PartidaMonopoly) partidaCargada);
+                            break;
+                    default:
+                            System.out.println("Poderoso caballero es Don Dinero, hasta luego!");
 
-	}
-		
-	
-}
+            }
+    }
+
+
+    
+
