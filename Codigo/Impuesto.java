bb371a6eb6341b7fc4e38c50dae771a921d85361
aronsalinas96 <32863687+aronsalinas96@users.noreply.github.com>
2017-11-29 22:03:06 +0100

    Update Impuesto.java

--- Codigo/Impuesto.java
+++ Codigo/Impuesto.java
@@ -1,8 +1,20 @@
-public class Impuesto extends CasillaMonopoly{
+package monopolyclasico;
+public class Impuesto extends Casilla{
 	int cantidad;
 
-	public Impuesto(String nombre, int cantidad){
+	public Impuesto(String nombre){
 		super(nombre);
-		this.cantidad = cantidad;
+		this.cantidad = 150;
 	}
+        
+    
+        public void accion(Jugador j){
+            System.out.println("Te toca pagar impuestos, pagas: "+cantidad);
+            j.setDinero(j.getDinero() - cantidad);
+        }
+
+        public void setCantidad(int c){this.cantidad = c;}
+        public void getCantidad(){return this.cantidad;}
+
+   
 }
