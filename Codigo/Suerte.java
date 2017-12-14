2bcd3c7aa865bf45076c5ff9bca050b74abb3fa0
aronsalinas96 <32863687+aronsalinas96@users.noreply.github.com>
2017-12-14 12:43:06 +0100

    Update Suerte.java
    Hay que terminar accion()

--- Codigo/Suerte.java
+++ Codigo/Suerte.java
@@ -15,16 +15,40 @@ public class Suerte extends Casilla{
 		return dAleatorio;
 	}
 	
-	public void accion(Jugador j){
+	public void accion(Jugador j,PartidaMonopoly p){
 		int a= (int)Math.floor(Math.random()*2+1);
 		
 		if(a==1){
+                        
                         int mueve = casillasAleatorio();
+                        if(j.getCasilla_actual() + mueve < 0){
+                            System.out.println("Te mueves a la casilla 0");
+                            j.setCasilla_actual(0);
+                        }
+                        else{
+                            
+                        
+                        System.out.println("Te mueves "+mueve+" casillas");
 			j.setCasilla_actual(j.getCasilla_actual()+mueve);
+                        }
 		}
 		else{
                         int dinero = dineroAleatorio();
-			j.setDinero(j.getDinero() + dinero);
+                        if(j.getDinero() + dinero < 0){
+                            if(p.sigueJugando(j,dinero) == false){
+                                p.eliminarJugador(j.getId(),"No tienes dinero ni propiedades que hipotecar");
+                            }
+                            else{
+                                //sumar valor todas propiedades
+                                //comprobar suma > deuda
+                                // si suma < deuda eliminar jugador
+                                //si no empezar a hipotecar hasta pagar deuda
+                            }
+                        }else{
+                            System.out.println("Recibes/pierdes "+dinero+" dinero");
+                            j.setDinero(j.getDinero() + dinero);
+                        }
+                       
 		}
 	}
 }
