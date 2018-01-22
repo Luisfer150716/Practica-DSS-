02ba45b7b16546014fcaf1b1c747986737378ff9
aronsalinas96 <32863687+aronsalinas96@users.noreply.github.com>
2018-01-23 00:05:18 +0100

    Update Propiedad.java

--- Codigo/Propiedad.java
+++ Codigo/Propiedad.java
@@ -7,7 +7,7 @@ public class Propiedad extends Casilla{
 	public Propiedad(String nombre, int valor){
 		super(nombre);
 		this.valor= valor;
-		this.dueño= null;
+		this.dueno= null;
 	}
 	public void accion(Jugador j, PartidaMonopoly p){
             System.out.println("Estas en la propiedad: "+this.getNombre());
